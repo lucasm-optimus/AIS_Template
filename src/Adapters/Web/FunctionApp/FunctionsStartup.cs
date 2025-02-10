6b955dbdac80f1f2fb3d408f243340a027de5077
Hamza Contractor <hamza.c@optimusinfo.com>
2025-02-10 12:09:55 +0530

    Add FunctionsStartup class for DI and ServiceBus setup
    Introduced FunctionsStartup class implementing IStartupRegister.
    Registered ServiceBusClient as a singleton using env variable.
    Added necessary using directives and new namespace.

---
+++ src/Adapters/Web/FunctionApp/FunctionsStartup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tilray.Integrations.Core.Common.Startup;
+
+namespace Tilray.Integrations.Functions
+{
+    public class FunctionsStartup : IStartupRegister
+    {
+        public IServiceCollection Register(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton(options => { return new ServiceBusClient(Environment.GetEnvironmentVariable("ServiceBusConnectionString")); });
+
+            return services;
+        }
+    }
+}
