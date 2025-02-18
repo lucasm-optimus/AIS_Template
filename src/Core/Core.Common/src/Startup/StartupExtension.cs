8aff797eba481de7778149592ab4925f91e6992b
sonalk-optimus <sonal.khatri@optimusinfo.com>
2025-02-18 14:03:12 +0530

    Add Functions Invocation logs

--- src/Core/Core.Common/src/Startup/StartupExtension.cs
+++ src/Core/Core.Common/src/Startup/StartupExtension.cs
@@ -74,6 +74,18 @@ public static class StartupExtensions
             .AddApplicationInsightsTelemetryWorkerService()
             .ConfigureFunctionsApplicationInsights();
 
+        builder.Logging.Services.Configure<LoggerFilterOptions>(options =>
+        {
+            // The Application Insights SDK adds a default logging filter that instructs ILogger to capture only Warning and more severe logs. Application Insights requires an explicit override.
+            // Log levels can also be configured using appsettings.json. For more information, see https://learn.microsoft.com/azure/azure-monitor/app/worker-service#ilogger-logs
+            LoggerFilterRule? defaultRule = options.Rules.FirstOrDefault(
+                rule => rule.ProviderName == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+            if (defaultRule is not null)
+            {
+                options.Rules.Remove(defaultRule);
+            }
+        });
+
         return builder;
     }
 
