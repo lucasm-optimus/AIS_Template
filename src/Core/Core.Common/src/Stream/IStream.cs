4b957a1d4cb2daad55c7eaffc9541bddd76438e0
sonalk-optimus <sonal.khatri@optimusinfo.com>
2025-02-21 17:08:36 +0530

    Update AzureServiceBusService.cs

--- src/Core/Core.Common/src/Stream/IStream.cs
+++ src/Core/Core.Common/src/Stream/IStream.cs
@@ -2,6 +2,7 @@
 
 public interface IStream
 {
-    Task SendEventAsync<T>(T notification, string topicName, Dictionary<string, object>? properties = null);
+    Task SendEventAsync<T>(T notification, string topicName);
+    Task SendEventAsync(string notification, string topicName, Dictionary<string, object>? properties = null);
     Task SendEventAsync<T>(T notification, string topicName, DateTime? scheduleMessage);
 }
